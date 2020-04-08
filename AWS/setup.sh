a35276c3d5a9e247efb1783039c57e1d7381a114
Joep de Ligt <joepio@gmail.com>
2020-04-08 12:11:51 +1200

    adding pseudo code for pipelines

---
+++ AWS/setup.sh
@@ -0,0 +1,6 @@
+## Setup environment on AWS
+git clone https://github.com/nextstrain/ncov.git
+git clone https://github.com/ESR-NZ/NZ_SARS-CoV-2_genomics.git
+
+mkdir -p external_data nz_data/genomes
+
