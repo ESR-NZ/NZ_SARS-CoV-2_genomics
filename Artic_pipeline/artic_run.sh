be5b30c933001ff154b78b87c3d4c3514e663a36
Matt Storey <matt.storey@esr.cri.nz>
2020-04-16 13:17:24 +1200

    artic_run added -c config

--- Artic_pipeline/artic_run.sh
+++ Artic_pipeline/artic_run.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
-##Set variables, hardcoded to work on production server with the strict directory structure.
+##Set variables
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -c|--config_file)
+    CONFIG="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    
+    POSITIONAL+=("$1")
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" 
+
 RUN_ID=$1
 
 
-## decide how to point to config file path
-source ./artic_pipeline_config
+source $CONFIG
+source $ACTIVATE $ARTIC_MEDAKA
+
+echo "Config file = ${CONFIG}"
+echo "DATA_DIR = ${DATA_DIR}"
+echo "ASSEMBLIES_DIR = ${ASSEMBLIES_DIR}"
 
 
-## check for the existance of command line arg
-[ -z "$1" ] && echo "No data directory supplied" && exit
+[ ! -z $CONFIG] && echo "Please suppply config file path" && exit 1
+[ ! -z $RUN_ID] && echo "Please suppply run-id" && exit 1
 ## exit if supplied directory is invalid
-[ ! -d $DATA_DIR/$RUN_ID ] && echo "$RUN_ID invalid data directory" && exit
+[ ! -d $DATA_DIR/$RUN_ID ] && echo "$RUN_ID invalid data directory" && exit 1
 ## Check if the data has already been processed, caution overwrite
 [ -d $ASSEMBLIES_DIR/${RUN_ID}_analysis ] && read -p "Warning! Analysis already exists for $RUN_ID. Press ENTER to continue and overwrite!"
 
@@ -55,8 +78,10 @@ CONSENSUN_DIR=$ANALYSIS_DIR/consensus
 mkdir $CONSENSUN_DIR
 cp $ANALYSIS_DIR/${RUN_ID}_assemblies/*.consensus.fasta $CONSENSUN_DIR
 
+
+
 ## Run the reporting script
-report.py --consensus_dir $CONSENSUN_DIR
+#report.py --consensus_dir $CONSENSUN_DIR --bucket $AWS_BUCKET
 
 
 echo ""
